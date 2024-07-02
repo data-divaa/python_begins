cef2fdd79eef1ed40600a75f94a23644547487c4
data-divaa <divya.1710.drishti@gmail.com>
2024-07-02 10:13:23 +0530

    check-in file 1st july

---
+++ 1-july-2024/max.py
@@ -0,0 +1,12 @@
+# maximum of 2 numbers if already given
+a = 66
+b = 65
+c = max(a,b)
+print(c)
+print("")
+print("")
+#maximum of 2 numbers if input is to be taken
+d = int(input("enter a number:    "))
+e = int(input("enter another number:    "))
+f = max(d,e)
+print("the maximum out of two numbers would be" , f)
--- 30-june-2024/first.py
+++ 30-june-2024/first.py
@@ -1,3 +1,31 @@
-# This is a new file
+# to recommend kdrama on the basis of perfernece
+def recommendation(romance,funny,historical,action):
+	if romance  and funny  and historical  and action :
+		print("According to you liking my recommendation would be- ALCHEMY OF SOULS ")
+		print("")
+		print("This is a historical series avaiable in netflix completed in two seasons it is a story based on soul shfiting asissain naksu")
+		print("and jang uk who is king star but is unknown to fact .Set in a fictional country called Daeho, it is about the love and growth of young mages as they overcome their twisted fates due to a forbidden magic spell known as the alchemy of souls, which allows souls to switch bodies.")
+		print("A powerful sorceress in a blind woman's body encounters a man from a prestigious family, who wants her help to change his destiny.")
+	elif not romance  and funny  and historical  and action:
+		print("According to you liking my recommendation would be- HOTEL DEL LUNA")
+		print("")
+		print("Jang Man-wol, the proprietor of a supernatural hotel, is bound to the place due to a grave sin. She soon hires Gu Chan-sung, a young man, as the manager and introduces him to a mysterious new world.")
+		print("this one does contain tiny bit of romance but not as its amin factor")
+	elif not romance  and not funny  and historical and action :
+		print("According to you liking my recommendation would be- JINGBIROK: A MEMOIR OF IMJIN WAR")
+		print("")
+		print("Ryu Seong Ryong, a historian and an official in King Seonjo's military, is assigned operations during the Imjin War where he influences Admiral Lee Soon Shin.")
+	elif not romance  and not funny  and historical  and  not action :
+		print("According to you liking my recommendation would be- STRANGER")
+		print("")
+		print("Hwang Si-mok, a prosecutor with lack of empathy, uses his wits and investigation skills to solve a murder case linked to corruption scandals inside South Korea's legal system")
+	elif romance and funny and not historical and action:
+		print("According to you liking my recommendation would be- DESCENDANTS OF SUN")
+		print("")
+		print("A soldier belonging to the South Korean Special Forces falls in love with a beautiful surgeon. However, their relationship is short-lived as their professions keep them apart.")
+	else :
+		print("According to you liking my recommendation would be- BUISNESS PROPOSAL")
+		print("")
+		print("In disguise as her friend, Ha-ri shows up on a blind date to scare away her friend's prospective suitor. However, plans go awry when he turns out to be Ha-ri's CEO and he makes a proposal.")
+return hope you enjoyed
 
-a = 24
